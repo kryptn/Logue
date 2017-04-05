38c20d39364fdb1eadd7ad77c15a051085e41b01
kryptn <kryptn@gmail.com>
2017-04-04 23:25:36 -0700

    Some JWT middleware, button counter route added

--- Logue/app.py
+++ Logue/app.py
@@ -1,26 +1,54 @@
+from functools import wraps
+
 import jwt
 from sanic import Sanic
 from sanic.response import json
 
 app = Sanic()
 
-secret = 'thesecret'
+SECRET = 'thesecret'
 
-def get_jwt(payload):
-    return jwt.encode(payload, secret, algorithm='HS256')
+empty_token = {'authenticated', False}
 
-def read_jwt(token):
-    return jwt.decode(token, secret)
 
-def build_jwt(payload):
-    return {'Authorization': 'Bearer {}'.format(get_jwt(payload))}
+class Token:
+
+    def __init__(self, token=None, data=None, algorithm='HS256'):
+        # will take token over data
+        if token:
+            data = self.load_token(token)
+        self.data = data if data else {}
+        self.algorithm = algorithm
+
+    def load_token(self, token):
+        return jwt.decode(token, SECRET)
+
+    @property
+    def token(self):
+        return jwt.encode(self.data, SECRET, algorithm=self.algorithm)
+
+    @property
+    def header(self):
+        return {'Authorization': 'Bearer: {}'.format(str(self.token, 'ASCII'))}
+
+
+@app.middleware('request')
+async def load_jwt(request):
+    request.args['Token'] = Token(token=request.token)
+
+@app.middleware('response')
+async def dump_jwt(request, response):
+    response.headers.update(request.args['Token'].header)
 
 @app.route('/')
 async def test(request):
-    return json({'message': 'Hello World'},
-            headers=build_jwt({'authed': False}),
-            status=200)
+    return json({'message': 'Hello World'})
 
+@app.route('/counter/')
+async def counter(request):
+    count = request.args['Token'].data.get('count', 0)
+    request.args['Token'].data['count'] = count + 1
+    return json({'message': "You've clicked {} times".format(count)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
